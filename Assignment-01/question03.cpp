120c7b5eee9192d73d19f58dfa91dddb6fcb9dc0
Ru-1234 <romaisaishaq037@gmail.com>
2025-02-20 23:16:01 +0500

    Add files via upload
    Determine whether a given 64-bit signed integer is even or odd under the following
    constraints:
    1. You cannot use:
    ○ Bitwise operators (&, |, ^, ~, <<, >>)
    ○ Comparison operators (>, <, >=, <=, ==, !=)
    ○ Logical operators (&&, ||, !)
    ○ String conversion or manipulation
    ○ Built-in functions that directly determine parity (like is_even(), mod(), etc.)
    ○ You must process the number in constant time (O(1) time complexity),
    regardless of its size.
    ○ The solution should work for both positive and negative integers.
    ○ You cannot precompute or hardcode any specific values (e.g., lookup tables).

---
+++ Assignment-01/question03.cpp
@@ -0,0 +1,20 @@
+//  Name:Romaisa    Roll No: 24I-0061(AI-B)
+
+#include<iostream>
+using namespace std;
+
+int main(){
+long long int num;
+
+cout<<"Enter 64-bit number: ";
+cin>>num;
+
+bool condition=num%2;
+
+if(condition)
+cout<<"\n"<<num<<" is Odd.";
+else
+cout<<"\n"<<num<<" is Even.";
+
+return 0;
+}
