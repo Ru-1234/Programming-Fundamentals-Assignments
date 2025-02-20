4a2603ca7c21718c7d61246b74ce31d1fc3f7c7f
Ru-1234 <romaisaishaq037@gmail.com>
2025-02-20 23:19:08 +0500

    Update question02.cpp

--- Assignment-01/question02.cpp
+++ Assignment-01/question02.cpp
@@ -506,7 +506,7 @@ int main(){
   
             return 0;
             }
-/*
+/*this logic could also be used:
 cin>>p1;
 if(p1=='?'){
   p2=' ',p3=' ',p4=' ',p5=' ',p6=' ',p7=' ',p8=' ',p9=' ',p10=' ',p11=' ',p12=' ',p13=' ',p14=' ',p15=' ',p16=' ';
